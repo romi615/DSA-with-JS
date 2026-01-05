fa978e1b1d8c1257bd7c29ac827b24f271b9587d
romi615 <romimawandia615@gmail.com>
2026-01-05 20:54:18 +0530

    add number-triangle

--- PatternQuestions/number-triangle.js
+++ PatternQuestions/number-triangle.js
@@ -1,8 +1,20 @@
-function numberTriangle(num){
+/*function numberTriangle(num){
     for(let i=1; i<=num; i++){
         let element = "";
         for(let j=1; j<=i; j++){
             element += j;
+        }
+        console.log(element);
+    }
+}
+numberTriangle(5);
+*/
+
+function numberTriangle(num){
+    for(let i=1; i<=num; i++){
+        let element = "";
+        for(let j=1; j<=i; j++){
+            element += i;
         }
         console.log(element);
     }
